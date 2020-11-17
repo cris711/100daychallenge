ffcc5cc965cc64184c2a2b22222f930f0a0c8189
CriiDawg <noemail@no.com>
2020-11-16 19:02:30 -0600

    update readme and code files

---
+++ lists and tuples.py
@@ -0,0 +1,5 @@
+x = input("please enter csv numbers (1,2,3,4):\n")
+list_view = x.split(",")
+tuple_view = tuple(list_view)
+print("list:",list_view)
+print("tuple:",tuple_view)
---
+++ name reverse.py
@@ -0,0 +1,4 @@
+inp = input("Please Enter your first name\n")
+inp2= input("Please Enter your last name\n")
+
+print("Reveresed:",inp2," ",inp)
